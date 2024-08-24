8d4b4f002cbe2a7f1f05dbe1b6fa7c91d9d2fb4c
YoyoJesus <51387449+YoyoJesus@users.noreply.github.com>
2024-08-24 15:11:35 -0400

    added button to 3lb rules

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,9 +18,16 @@ function HomepageHeader() {
         <img src='./USINGimg/longlogo.png'></img>
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className='row'>
-        </div>
         <br/>
+        <div className='row'>
+        <div style={{ textAlign: 'center', width: '50%', margin: 'auto'}}>
+            <Link to='/blog/2024minicomprules'>
+              <button style={{ fontSize: '30px', padding: '20px 30px' }}>
+                3lb Competition Information
+              </button>
+            </Link>
+          </div> 
+        </div>
         <br/>
         <p className="hero__subtitle">We meet every Friday from 5-7pm in 120 AEB. <br/> Being a club member is <i>ONLY</i> available to KSU Students. </p>
         {/*<p>*Quick Note: This site is best viewed under the dark mode settings. Please click the little sun icon in the top right corner.</p>*/}
