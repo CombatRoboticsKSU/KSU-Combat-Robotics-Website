19c942f22f0a7d383b52cc050620056bb57c3be0
Ender <nallahabi2003@gmail.com>
2024-12-17 22:13:40 -0500

    Converted the list of sponsors into a dynamic slide-show

--- src/pages/sponsorship.tsx
+++ src/pages/sponsorship.tsx
@@ -10,6 +10,12 @@ import styles from './index.module.css';
 //need to add information about how to sponsor us (sponsorship packages, etc)
 //add SKB Cases after Friday meeting (9/20/24)
 
+// imported these to allow for a basic interactive slide show
+import React from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
@@ -29,7 +35,41 @@ function HomepageHeader() {
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
-  const w = 500;
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    customPaging: (i) => ( // Custom dot button styling
+      <button
+        style={{
+          width: "10px",
+          height: "10px",
+          borderRadius: "50%",
+          background: "gray",
+          border: "none",
+          margin: "5px",
+          cursor: "pointer",
+        }}
+      />
+    ),
+  };
+
+  // Add new sponsors when needed, the slider will handle the heavy lifting
+  // the react-js slider is dynamic
+  const sponsors = [
+    { link: 'https://www.wardjet.com', src: '/USINGimg/WARDjet.jpg', alt: 'WARDjet' },
+    { link: 'https://itgresa.com', src: '/USINGimg/ITGresa.jpg', alt: 'IT Gresa' },
+    { link: 'https://badasspower.com', src: '/USINGimg/BadAss.jpg', alt: 'Bad-Ass Motors' },
+    { link: 'https://repeat-robotics.com', src: '/USINGimg/repeat_g.jpg', alt: 'Repeat Robotics' },
+    { link: 'https://sendcutsend.com', src: '/USINGimg/send_g.jpg', alt: 'SendCutSend' },
+    { link: 'https://www.haascnc.com/content/ghf/en/home.html', src: '/USINGimg/haascnc.jpg', alt: 'Gene Haas Foundation' },
+    { link: 'https://www.skbcases.com', src: '/USINGimg/skb.jpg', alt: 'SKB Cases' },
+  ];
+
   return (
     <Layout
       title={`Sponsorship`}
@@ -40,23 +80,22 @@ export default function Home(): JSX.Element {
           <p className='hero__title'>Current Sponsors:</p>
         </div>
 
-        <div className='sponsorshipPage' style={{ textAlign: 'center'}}>
-          <ul style={{ listStyleType: 'none', padding: '0px' }}>
-            <li><a href='https://www.wardjet.com'                           target="_blank"><img src='/USINGimg/WARDjet.jpg'  alt='WARDjet'               width={w} height="auto"></img></a></li>
-            <li><a href='https://itgresa.com'                               target="_blank"><img src='/USINGimg/ITGresa.jpg'  alt='IT Gresa'              width={w} height="auto"></img></a></li>
-            <li><a href='https://badasspower.com'                           target="_blank"><img src='/USINGimg/BadAss.jpg'   alt='Bad-Ass Motors'        width={w} height="auto"></img></a></li>
-            <li><a href='https://repeat-robotics.com'                       target="_blank"><img src='/USINGimg/repeat_g.jpg' alt='Repeat Robotics'       width={w} height="auto"></img></a></li>
-            <li><a href='https://sendcutsend.com'                           target="_blank"><img src='/USINGimg/send_g.jpg'   alt='SendCutSend'           width={w} height="auto"></img></a></li>
-            <li><a href='https://www.haascnc.com/content/ghf/en/home.html'  target="_blank"><img src='/USINGimg/haascnc.jpg'  alt='Gene Haas Foundation'  width={w} height="auto"></img></a></li>
-            <li><a href='https://www.skbcases.com'                          target="_blank"><img src='/USINGimg/skb.jpg'      alt='SKB Cases'             width={w} height="auto"></img></a></li>
-          </ul>
+        <div className='sponsorshipPage' style={{ textAlign: 'center' }}>
+          <Slider {...settings}>
+            {sponsors.map((sponsor, index) => (
+              <div key={index}>
+                <a href={sponsor.link} target="_blank" rel="noopener noreferrer">
+                  <img src={sponsor.src} alt={sponsor.alt} style={{ width: "500px", margin: "0 auto" }} />
+                </a>
+              </div>
+            ))}
+          </Slider>
         </div>
 
         <div className='sponsorshipPage' style={{ textAlign: 'center', padding: '20px' }}>
           <h1>Interested in Sponsoring Our Team?</h1>
           <p>Click <u><a href="/img/Letter.pdf" target="_blank">here</a></u> for our Sponsorship Packet</p>
         </div>
-
       </main>
     </Layout>
   );
