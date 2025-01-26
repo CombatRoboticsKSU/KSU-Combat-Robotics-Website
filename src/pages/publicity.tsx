e91b032b50d4e9eec31bcae006879adfdfc41f6b
Ender <nallahabi2003@gmail.com>
2025-01-26 13:55:18 -0500

    Changed the text display styling

--- src/pages/publicity.tsx
+++ src/pages/publicity.tsx
@@ -45,82 +45,88 @@ export default function Home(): JSX.Element {
             {/*
               NEWS items go in this div
             */}
-            <a href='https://www.kent.edu/cae/news/kent-state-xbots-nhrl-robot-combat-league'>
-              <img
-                  src="/USINGimg/xbots_team.jpg"
-                  alt="Kent State XBots at the NHRL Robot Combat League"
-                  style={{ width: '400px', marginRight: '20px', borderRadius: '8px' }}
-              />
-            </a>
             <section style={{ display: 'flex', alignItems: 'center', marginBottom: '40px' }}>
-              <div>
+              <a href='https://www.kent.edu/cae/news/kent-state-xbots-nhrl-robot-combat-league'>
+                <img
+                    src="/USINGimg/xbots_team.jpg"
+                    alt="Kent State XBots at the NHRL Robot Combat League"
+                    style={{ width: '400px', marginRight: '20px', borderRadius: '8px' }}
+                />
+              </a>
+              <div style={{ margin: '30px' }}>
                 <Heading as="h2">Kent State XBots at the NHRL Robot Combat League (May 1, 2022)</Heading>
                 <p>
-                In March, they competed at the Norwalk Havoc Robot League (NHRL) Robot Combat League in Norwalk, Connecticut. Kent State XBots were able to fight their way to the Semi-Finals, which is something worth bragging about!
+                In March, they competed at the Norwalk Havoc Robot League (NHRL) Robot Combat League in Norwalk, Connecticut.
+                Kent State XBots were able to fight their way to the Semi-Finals, which is something worth bragging about!
                 </p>
               </div>
             </section>
 
-            <a href='https://www.kent.edu/cae/news/combat-robotics-team-finds-success-first-nhrl-season'>
-              <img
-                  src="/USINGimg/successInFirstNHRLSeason.png"
-                  alt="Success in first NHRL Season"
-                  style={{ width: '400px', marginRight: '20px', borderRadius: '8px' }}
-              />
-            </a>
             <section style={{ display: 'flex', alignItems: 'center', marginBottom: '40px' }}>
-              <div>
+              <a href='https://www.kent.edu/cae/news/combat-robotics-team-finds-success-first-nhrl-season'>
+                <img
+                    src="/USINGimg/successInFirstNHRLSeason.png"
+                    alt="Success in first NHRL Season"
+                    style={{ width: '400px', marginRight: '20px', borderRadius: '8px' }}
+                />
+              </a>
+              <div style={{ margin: '30px' }}>
                 <Heading as="h2">Combat Robotics Team finds Success in first NHRL Season (February 13, 2023)</Heading>
                 <p>
-                On November 12th, 2022, the Kent State University Combat Robotics Team placed 3rd at a Norwalk Havoc Robotics League (NHRL) competition, qualifying for the 2022 Championships in December 2023.
+                On November 12th, 2022, the Kent State University Combat Robotics Team placed 3rd at a
+                Norwalk Havoc Robotics League (NHRL) competition, qualifying for the 2022 Championships in December 2023.
                 </p>
               </div>
             </section>
 
-            <a href='https://kentstater.com/95378/uncategorized/combat-robotics-team-continues-to-find-success/'>
-              <img
-                  src="/USINGimg/FlashBang.jpg"
-                  alt="Success Continues at NHRL"
-                  style={{ width: '400px', marginRight: '20px', borderRadius: '8px' }}
-              />
-            </a>
             <section style={{ display: 'flex', alignItems: 'center', marginBottom: '40px' }}>
-              <div>
+              <a href='https://kentstater.com/95378/uncategorized/combat-robotics-team-continues-to-find-success/'>
+                <img
+                    src="/USINGimg/FlashBang.jpg"
+                    alt="Success Continues at NHRL"
+                    style={{ width: '400px', marginRight: '20px', borderRadius: '8px' }}
+                />
+              </a>
+              <div style={{ margin: '30px' }}>
                 <Heading as="h2">Combat Robotics Team Continues to find Success (March 29, 2023)</Heading>
                 <p>
-                At the start of the semester the members of the Combat Robotics team weren’t sure whether they would be able to compete at any more events because of a lack of funding, but thanks to a generous donation from the NHRL, the team has continued competing.
+                At the start of the semester the members of the Combat Robotics team weren’t sure whether they would be able
+                to compete at any more events because of a lack of funding, but thanks to a generous donation from the NHRL, the team has continued competing.
                 </p>
               </div>
             </section>
 
-            <a href='https://www.kent.edu/cae/news/combat-robotics-team-makes-2023-nhrl-season-debut'>
-              <img
-                  src="/USINGimg/Flashbang_w_Minibot.jpg"
-                  alt="NHRL 2023 Season-Debut"
-                  style={{ width: '400px', marginRight: '20px', borderRadius: '8px' }}
-              />
-            </a>
             <section style={{ display: 'flex', alignItems: 'center', marginBottom: '40px' }}>
-              <div>
+              <a href='https://www.kent.edu/cae/news/combat-robotics-team-makes-2023-nhrl-season-debut'>
+                <img
+                    src="/USINGimg/Flashbang_w_Minibot.jpg"
+                    alt="NHRL 2023 Season-Debut"
+                    style={{ width: '400px', marginRight: '20px', borderRadius: '8px' }}
+                />
+              </a>
+              <div style={{ margin: '30px' }}>
                 <Heading as="h2">Combat Robotics Team Makes 2023 NHRL Season Debut (April 20, 2023)</Heading>
                 <p>
-                At the start of the semester the members of the Combat Robotics team weren’t sure whether they would be able to compete at any more events because of a lack of funding, but thanks to a generous donation from the NHRL, the team has continued competing.
+                At the start of the semester the members of the Combat Robotics team weren’t sure whether they would
+                be able to compete at any more events because of a lack of funding, but thanks to a generous donation from the NHRL, the team has continued competing.
                 </p>
               </div>
             </section>
 
-            <a href='https://www.kent.edu/cae/news/combat-robotics-team-takes-robotics-great-nhrl-competition'>
-              <img
-                  src="/USINGimg/NHRL_June_Team.jpg"
-                  alt="Combat Robotics takes on the Great NHRL Competition"
-                  style={{ width: '400px', marginRight: '20px', borderRadius: '8px' }}
-              />
-            </a>
             <section style={{ display: 'flex', alignItems: 'center', marginBottom: '40px' }}>
-              <div>
+              <a href='https://www.kent.edu/cae/news/combat-robotics-team-takes-robotics-great-nhrl-competition'>
+                <img
+                    src="/USINGimg/NHRL_June_Team.jpg"
+                    alt="Combat Robotics takes on the Great NHRL Competition"
+                    style={{ width: '400px', marginRight: '20px', borderRadius: '8px' }}
+                />
+              </a>
+              <div style={{ margin: '30px' }}>
                 <Heading as="h2">Combat Robotics Team Takes on Robotics Great at NHRL Competition (July 13, 2023)</Heading>
                 <p>
-                In June, the Kent State University Combat Robotics Team took on another weekend of fierce competition in the National Havoc Robotics League. After making some key modifications to their bot Flash-BANG, which made its NHRL debut in March, the team was prepared to face their biggest challenge yet.
+                In June, the Kent State University Combat Robotics Team took on another weekend of fierce competition
+                in the National Havoc Robotics League. After making some key modifications to their bot Flash-BANG,
+                which made its NHRL debut in March, the team was prepared to face their biggest challenge yet.
                 </p>
               </div>
             </section>
