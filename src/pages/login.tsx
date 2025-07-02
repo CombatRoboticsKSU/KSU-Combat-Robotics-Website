bf53a882a1dfea30973408cf8639b0ac7b829eb7
Ian Rohrbacher <ianjay1026@gmail.com>
2025-07-01 21:16:46 -0400

    backend?

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { FaDiscord } from 'react-icons/fa';
 
 // Centralized API base URL
-const API_BASE_URL = 'http://localhost:4000';
-//const API_BASE_URL = '/api';
+//const API_BASE_URL = 'http://localhost:4000';
+const API_BASE_URL = 'api';
 
 function Login() {
     const [user, setUser] = useState<any>(null);
