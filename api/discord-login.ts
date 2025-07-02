0d68b56be311d9e43d438edebaaaf52cf228720a
Ian Rohrbacher <ianjay1026@gmail.com>
2025-07-01 23:22:27 -0400

    Update discord-login.ts

--- api/discord-login.ts
+++ api/discord-login.ts
@@ -1,8 +1,8 @@
 function handler(req, res) {
   const clientId = process.env.DISCORD_CLIENT_ID;
-  const redirectUri = encodeURIComponent(`${process.env.NEXT_PUBLIC_BASE_URL}/api/discord-callback`);
+  const redirectUri = `${process.env.NEXT_PUBLIC_BASE_URL}/api/discord-callback`;
   const scope = 'identify guilds guilds.members.read';
-  const discordAuthUrl = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${encodeURIComponent(scope)}`;
+  const discordAuthUrl = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=${encodeURIComponent(scope)}`;
   res.redirect(discordAuthUrl);
 }
 
