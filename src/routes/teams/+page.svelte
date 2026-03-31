d2ccdd96dd6d91c5251e056a49d10b4332649e24
Yoyojesus <austinplaysmms12@gmail.com>
2026-03-31 19:43:26 -0400

    teams

--- src/routes/teams/+page.svelte
+++ src/routes/teams/+page.svelte
@@ -4,17 +4,17 @@
 
 	onMount(() => {
 		if (browser) {
-			window.location.href = 'https://www.discord.gg/qxqDEgcKns';
+			window.location.href = 'https://teams.microsoft.com/l/team/19%3AAuUT5tyjbeWyBIqBtbaxXsHSR3LuFIDW_VicpuGOWOo1%40thread.tacv2/conversations?groupId=cd19e8af-e1e0-4385-9c5a-1f68a121e687&tenantId=e5a06f4a-1ec4-4d01-8f73-e7dd15f26134';
 		}
 	});
 </script>
 
 <svelte:head>
-	<title>Discord | KSU Combat Robotics</title>
+	<title>Teams | KSU Combat Robotics</title>
 </svelte:head>
 
 <section class="redirect">
-	<p>Redirecting to Discord...</p>
+	<p>Redirecting to Teams...</p>
 </section>
 
 <style>
